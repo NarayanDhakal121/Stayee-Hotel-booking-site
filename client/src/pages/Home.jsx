2f89ae4a89920f31a4cee0d4ec76f50325caee0e
NarayanDhakal121 <www.narine121@gmail.com>
2025-06-20 13:10:24 +0545

    Explore destination and testimonials

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,16 @@
 import React from "react";
 import Hero from "../components/Hero";
 import FeaturedDestination from "../components/FeaturedDestination";
+import ExclusiveOffer from "../components/ExclusiveOffer";
+import Testimonials from "../components/Testimonials";
 
 const Home = () => {
   return (
     <>
       <Hero />
-      <FeaturedDestination/>
+      <FeaturedDestination />
+      <ExclusiveOffer />
+      <Testimonials />
     </>
   );
 };
